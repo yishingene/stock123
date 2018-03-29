d41971e94d2e572444025dd3f17f6acca367a827
rocky.wang <rockywang101@gmail.com>
2018-03-29 12:17:38 +0800

    1

---
+++ fetch_some_stock_data.py
@@ -0,0 +1,23 @@
+'''
+Created on 2018年3月29日
+@author: rocky.wang
+'''
+
+from twse_crawler import TWSECrawler
+import time
+
+def main():
+
+    crawler = TWSECrawler()
+    
+    # 爬大盤資料寫入 csv 檔
+    crawler.fetchStockInfo("t00")
+    time.sleep(5)
+    crawler.fetchStockInfo("0050")
+    time.sleep(5)
+    crawler.fetchStockInfo("0056")
+    
+    
+if __name__ == "__main__":
+    main()
+

960f5170fded184b578a8d3f4aefe2aa17d15f38
rocky.wang <rockywang101@gmail.com>
2018-03-29 12:12:21 +0800

    1

--- fetch_all_stock_data.py
+++ fetch_all_stock_data.py
@@ -5,11 +5,29 @@ Created on 2018年3月27日
 @author: rocky.wang
 '''
 from twse_crawler import TWSECrawler
+import datetime
+import time
 
 def main():
-    # 爬資料寫到各 csv 檔
+
+    now = datetime.datetime.now()
+    
     crawler = TWSECrawler()
-    crawler.fetchAllStockFinalData()
+    
+    dt = datetime.datetime(2018, 3, 28)
+    
+    if int(str(now.hour) + format(now.minute, "02")) >= 1207:
+        # 爬資料寫到各 csv 檔
+        crawler.fetchAllStockFinalData(dt)
+#         crawler.fetchAllStockFinalData()
+      
+    print("sleeping 5 seconds...")
+    time.sleep(5)
+      
+    # 爬大盤資料寫入 csv 檔
+    crawler.fetchStockInfo("t00")
+    
+    
     
 if __name__ == "__main__":
     main()
--- twse_crawler.py
+++ twse_crawler.py
@@ -72,20 +72,46 @@ class TWSECrawler():
                 else:
                     return None
 
-
-    def fetchStockInfo(self, stockId):
+    ''' 單純想爬最新資料 '''
+    def crawlStockInfo(self, stockId):
         s = requests.Session()
         s.get("http://mis.twse.com.tw/stock/index.jsp")
         url = "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_{stockId}.tw&_={time}".format(stockId=stockId, time=int(time.time()) * 1000)
         r = s.get(url)
-        print("\nGET {}\n{}".format(url, r.text.strip()))
+        print("GET URL => {}\nResponse => {}".format(url, r.text.strip().replace("False", "false")))
         return r.json()
-
-    def fetchAllStockData(self):
         
-        pass
-    
-    
+
+    ''' 爬最新資料並寫回 csv 檔案 '''
+    def fetchStockInfo(self, stockId):
+
+        # 讀舊資料出來
+        rowDict = {}
+        with open("data/{}.csv".format(stockId)) as f1:
+            for row in csv.reader(f1):
+                rowDict[row[0]] = row
+        
+        js = self.crawlStockInfo(stockId)
+        # 先不考慮錯的時候，直接讓它丟出，最外面直接 line notify 錯誤
+#         if js["rtcode"] == "0000":
+        v = int(js["msgArray"][0]["v"]) # 成交股數
+        z = float(js["msgArray"][0]["z"]) # 現價
+        y = float(js["msgArray"][0]["y"]) # 昨日價
+        
+        dt = "{}/{}/{}".format(js["msgArray"][0]["d"][0:4], js["msgArray"][0]["d"][4:6], js["msgArray"][0]["d"][6:8])
+        
+        row = [dt, v, int(v*z), js["msgArray"][0]["o"], js["msgArray"][0]["h"], js["msgArray"][0]["l"], js["msgArray"][0]["z"], round(z-y, 2), "", "", ""]
+        rowDict[row[0]] = row
+                
+        with open("data/{}.csv".format(stockId), "w", newline="") as f1:
+            writer = csv.writer(f1)
+            for d in rowDict.values():
+                writer.writerow(d)
+                
+        self.appendData(stockId)
+
+
+    ''' 收盤後，爬所有收盤股票資料 '''    
     def fetchAllStockFinalData(self, dt=datetime.datetime.now()):
         
         url = "http://www.twse.com.tw/exchangeReport/MI_INDEX?response=json&date=%s&type=ALLBUT0999&_=%s" %(dt.strftime("%Y%m%d"), int(time.time()*1000))
@@ -107,7 +133,7 @@ class TWSECrawler():
                 sign = '-' if data[9].find('green') > 0 else ''
                 
             date = "{}/{}/{}".format(js["date"][0:4], js["date"][4:6], js["date"][6:8])
-            row = [date, data[2], data[4], data[5], data[6], data[7], data[8], sign+data[10], data[3]]
+            row = [date, data[2], data[4], data[5], data[6], data[7], data[8], sign+data[10], data[3], "", ""] # 後面兩個空值是 RSV & K9
             row = self._make_datatuple(row)
             if not os.path.exists("data/{}.csv".format(stockId)):
 #                 with open("data/{}.csv".format(stockId), "a", newline="") as f1:    
@@ -116,7 +142,7 @@ class TWSECrawler():
                 # 先略過，不處理，之後再處理這些新的
                 continue
             
-            # 先讀出舊的資料
+            # 讀出舊的資料
             rowDict = {}
             with open("data/{}.csv".format(stockId), "r") as f1:
                 for row1 in csv.reader(f1):
@@ -128,6 +154,64 @@ class TWSECrawler():
                 writer = csv.writer(f1)
                 for d in rowDict.values():
                     writer.writerow(d)
+            
+            # 更新 RSV & K9
+            self.appendData(stockId)
+
+
+    ''' append RSV & K9 data '''
+    def appendData(self, stockId):
+        
+        print("Process append RSV & K9 data for {}".format(stockId))
+        
+        maxList = [0, 0, 0, 0, 0, 0, 0, 0, 0] # 9 天的最高價
+        minList = [0, 0, 0, 0, 0, 0, 0, 0, 0] # 9 天的最低價
+        k9 = 50
+        cnt = 1
+        newRowList = []
+        
+        with open("data/{}.csv".format(stockId), "r") as csvfile:
+            reader = csv.reader(csvfile)
+            newRowList.append(next(reader)) # header
+            for row in reader:
+                if row[4] == '':
+                    continue
+                
+                maxList.pop(0)
+                maxList.append(float(row[4])) # 最高價
+                minList.pop(0)
+                minList.append(float(row[5])) # 最低價
+        
+                try:
+                    rsv = round((100 * (float(row[6]) - min(minList)) / (max(maxList) - min(minList))), 2)
+                except ZeroDivisionError:
+                    rsv = 0
+                    
+                # 前 9 天的 K9 值都為 50
+                if cnt >= 10:
+                    # 更新，發現 yahoo 的算法應該是先四捨五入後再相加
+                    v1 = round(rsv / 3, 2)
+                    v2 = round(float(k9) * 2 / 3, 2)
+                    k9 = round(v1+v2, 2) # 兩個都已經四捨五入，但相加還是可能會有無限小數，python 太奧妙了
+                    k9 = format(k9, ".2f") # 在 linux 上跑 round 會無效
+                    
+                if len(row) >= 10:
+                    row[9] = rsv
+                else:
+                    row.append(rsv)
+                    
+                if len(row) >= 11:
+                    row[10] = k9
+                else:
+                    row.append(k9)
+                    
+                newRowList.append(row)
+                cnt += 1
+        
+        with open("data/{}.csv".format(stockId), "w", newline="\n") as csvfile:
+            writer = csv.writer(csvfile)
+            for row in newRowList:
+                writer.writerow(row)
 
 
 if __name__ == "__main__":
@@ -137,7 +221,10 @@ if __name__ == "__main__":
 #     dt = datetime.datetime(2018, 3, 27)
         
     cr = TWSECrawler()
-    cr.fetchAllStockFinalData(dt)
+    
+#     cr.fetchAllStockFinalData(dt)
+    
+    js = cr.fetchStockInfo("0056")
     
     
     

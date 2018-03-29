7310d9f1071cef9fc72349da2ad618d01b675d18
rocky.wang <rockywang101@gmail.com>
2018-03-29 13:50:22 +0800

    1

--- fetch_some_stock_data.py
+++ fetch_some_stock_data.py
@@ -6,15 +6,18 @@ Created on 2018年3月29日
 from twse_crawler import TWSECrawler
 import time
 
+import sys
+sys.path.append("/data/data/com.termux/files/home/stock123")
+
 def main():
 
     crawler = TWSECrawler()
     
     # 爬大盤資料寫入 csv 檔
     crawler.fetchStockInfo("t00")
-    time.sleep(5)
+    time.sleep(2)
     crawler.fetchStockInfo("0050")
-    time.sleep(5)
+    time.sleep(2)
     crawler.fetchStockInfo("0056")
     
     

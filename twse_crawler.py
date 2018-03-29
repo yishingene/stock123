291e0e15b499352e11d8b20e54da7b0323870137
rocky.wang <rockywang101@gmail.com>
2018-03-29 15:50:14 +0800

    1

--- twse_crawler.py
+++ twse_crawler.py
@@ -217,61 +217,6 @@ class TWSECrawler():
                 writer.writerow(row)
 
 
-    ''' append RSV & K9 data '''
-    def appendData(self, stockId):
-        
-        print("Process append RSV & K9 data for {}".format(stockId))
-        
-        maxList = [0, 0, 0, 0, 0, 0, 0, 0, 0] # 9 天的最高價
-        minList = [0, 0, 0, 0, 0, 0, 0, 0, 0] # 9 天的最低價
-        k9 = 50
-        cnt = 1
-        newRowList = []
-        
-        with open("data/{}.csv".format(stockId), "r", encoding="MS950") as csvfile:
-            reader = csv.reader(csvfile)
-            newRowList.append(next(reader)) # header
-            for row in reader:
-                if row[4] == '':
-                    continue
-                
-                maxList.pop(0)
-                maxList.append(float(row[4])) # 最高價
-                minList.pop(0)
-                minList.append(float(row[5])) # 最低價
-        
-                try:
-                    rsv = round((100 * (float(row[6]) - min(minList)) / (max(maxList) - min(minList))), 2)
-                except ZeroDivisionError:
-                    rsv = 0
-                    
-                # 前 9 天的 K9 值都為 50
-                if cnt >= 10:
-                    # 更新，發現 yahoo 的算法應該是先四捨五入後再相加
-                    v1 = round(rsv / 3, 2)
-                    v2 = round(float(k9) * 2 / 3, 2)
-                    k9 = round(v1+v2, 2) # 兩個都已經四捨五入，但相加還是可能會有無限小數，python 太奧妙了
-                    k9 = format(k9, ".2f") # 在 linux 上跑 round 會無效
-                    
-                if len(row) >= 10:
-                    row[9] = rsv
-                else:
-                    row.append(rsv)
-                    
-                if len(row) >= 11:
-                    row[10] = k9
-                else:
-                    row.append(k9)
-                    
-                newRowList.append(row)
-                cnt += 1
-        
-        with open("data/{}.csv".format(stockId), "w", newline="\n", encoding="MS950") as csvfile:
-            writer = csv.writer(csvfile)
-            for row in newRowList:
-                writer.writerow(row)
-
-
 if __name__ == "__main__":
 
     dt = datetime.datetime.now()

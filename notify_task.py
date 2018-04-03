0f74db83ad4aab834f44673c0e0f9a174ac83fee
rocky.wang <rockywang101@gmail.com>
2018-04-03 15:54:10 +0800

    1

--- notify_task.py
+++ notify_task.py
@@ -58,17 +58,17 @@ def main():
     print(msg)
 
     # 發 LINE 通知
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN"], msg)
-#     time.sleep(2)   # delays for n seconds
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN2"], msg)
-#     time.sleep(2)
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN3"], msg)
-#     time.sleep(2)
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN4"], msg)
-#     time.sleep(2)
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN5"], msg)
-#     time.sleep(2)
-#     lineTool.lineNotify(os.environ["LINE_0050_TOKEN6"], msg)
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN"], msg)
+    time.sleep(2)   # delays for n seconds
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN2"], msg)
+    time.sleep(2)
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN3"], msg)
+    time.sleep(2)
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN4"], msg)
+    time.sleep(2)
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN5"], msg)
+    time.sleep(2)
+    lineTool.lineNotify(os.environ["LINE_0050_TOKEN6"], msg)
     
 #     lineTool.lineNotify(os.environ["LINE_TEST_TOKEN"], msg)
 

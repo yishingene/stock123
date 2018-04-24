5a790890b30a7cf29750ac6066ed9802abe65524
rocky.wang <rockywang101@gmail.com>
2018-04-24 18:18:32 +0800

    1

--- testStockAssetsBzPerformance.py
+++ testStockAssetsBzPerformance.py
@@ -8,16 +8,41 @@ Created on 2018年4月8日
 '''
 from testStockAssetsStatus import fetchStockAssetsStatus
 from testStockBzPerformance import fetchStockBzPerformance
-
+ 
 stockId = "1264"
-stockId = "1215"
-stockId = "2002"
-
+ 
 longTermDebit = fetchStockAssetsStatus(stockId)
 pureEarn = fetchStockBzPerformance(stockId)
-
+  
 rate = round(longTermDebit / pureEarn, 2)
-
+  
 print(longTermDebit, pureEarn, rate)
+
+
+# import os
+# import csv
+# 
+# for filename in os.listdir("data_assetsStatus"):
+#     stockId = filename.split(".")[0]
+#     
+#     # file is empty
+#     if os.stat("data_assetsStatus/{}".format(filename)).st_size == 0:
+#         continue
+#     
+#     if filename != "1264.csv":
+#         continue
+#     
+#     with open("data_assetsStatus/{}".format(filename), encoding="utf-8") as f1:
+#         print(filename)
+#         row = list(csv.reader(f1))[0]
+#         print(row[18])
+        
+    
+    
+    
+    
+    
+
+
 
 

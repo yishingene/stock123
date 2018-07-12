4051c653856c04dd5dd7f2afb4897bf80dbab8be
Rocky <rockywang101@gmail.com>
2018-07-12 17:06:04 +0800

    1

--- latestNews/testLatestNews3.py
+++ latestNews/testLatestNews3.py
@@ -1,6 +1,6 @@
-'''
-test detail
+# -*- coding: utf-8 -*-
 
+'''
 Created on 2018年7月7日
 @author: rocky.wang
 '''
@@ -46,36 +46,42 @@ def fetch():
         stime = tds[3].text
         title = tds[4].text
         
-#         print(sid, sname, sdate, stime, title)
+        print(sid, sname, sdate, stime, title)
 
-        detailUrl = "http://mops.twse.com.tw/mops/web/ajax_t05sr01_1"
-        postData = composePostData(tds[5].find('input').get("onclick"))
-        
-        r = requests.post(detailUrl, data = postData)
+        txt = tds[5].find('input')
+
+        headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+                'Referer' : url
+        }
+
+        post_url = 'http://mops.twse.com.tw/mops/web/ajax_t05sr01_1'
+        postData = {
+                'TYPEK' : 'all', 
+                'step' : '1' ,
+                'skey' : getPostValus(txt.get('onclick'), 'skey' ),
+                'hhc_co_name' : getPostValus(txt.get('onclick'), 'hhc_co_name' ),
+                'COMPANY_ID' : getPostValus(txt.get('onclick'), 'COMPANY_ID' ),
+                'COMPANY_NAME' : getPostValus(txt.get('onclick'), 'COMPANY_NAME' ),
+                'SPOKE_DATE' : getPostValus(txt.get('onclick'), 'SPOKE_DATE' ),
+                'SPOKE_TIME' : getPostValus(txt.get('onclick'), 'SPOKE_TIME' ),
+                'SEQ_NO' : getPostValus(txt.get('onclick'), 'SEQ_NO' ),
+        }
+
+        r = requests.post(post_url, data = postData, headers = headers)
         r.encoding = "utf-8"
- 
+
         soup = BeautifulSoup(r.text, "html.parser")
         td = soup.find("td", {"style": "!important;text-align:left; !important;"})
         print(td.text)
-         
+        
         row = [sid, sname, sdate, stime, title, td.text]
         rowList.append(row)
-    
-        break
-
-def composePostData(onclickValue):
-    
-    postData = {
-        'TYPEK' : 'all', 
-        'step' : '1'
-    }
         
-    for co in onclickValue.split(";"):
-        if not co.startswith("document.fm_t05sr01_1"):
-            continue
-        co = co.replace("document.fm_t05sr01_1.", "").replace(".value", "").replace("'", "")
-        postData[co.split("=")[0]] = co.split("=")[1]
+        time.sleep(3)
     
-    return postData
-    
+    with open("news.csv", "w", encoding="utf-8", newline="") as f1:
+        writer = csv.writer(f1)
+        writer.writerows(rowList)    
+        
 fetch()
